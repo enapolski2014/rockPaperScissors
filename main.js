feab2953115e5fcc9cbab813851f9be619cee2e1
enapolski2014 <enapolski2014@gmail.com>
2025-09-23 22:39:10 -0500

    Created html and js, started pseudocode for js

---
+++ main.js
@@ -0,0 +1,41 @@
+/*
+Logic for rockpaper scissors computer choice:
+create function for getComputerChoice
+assign a variable that = computer choice
+
+computer choice runs when human choice is input into console
+
+    Math.random returns number >= 0 and <= 1
+    computer chooses 1 of three options
+        if >= 0 and <= .33, return rock
+        if > .33 and <= .66, return paper
+        if > .66 and <= .99, return scissors
+
+*/
+
+let computerChoice = ;
+
+let getComputerChoice() {
+
+}
+
+/*
+Logic for human choice:
+create a funciton for getHumanChoice
+Use prompt method to get human choice
+Human choice will return one of the valid choices 
+    depending on what the user inputs
+
+*/
+
+/*
+Declare player score variables
+Create two new variables named humanScore and computerScore in the GLOBAL SCOPE
+Initialize those variables with a value of 0
+*/
+
+/*
+Logic to play a single round
+Create function named playRound()
+
+*/
