f3d073564efce6b4afdf2eac7701531f7f73b79c
enapolski2014 <enapolski2014@gmail.com>
2025-10-12 15:50:48 -0500

    Added playRound() to playGame(), made it work

--- main.js
+++ main.js
@@ -13,22 +13,9 @@ computer choice runs when human choice is input into console
 
 */
 
-const computerSelection = getComputerChoice();
 
-const humanSelection = getHumanChoice();
 
-function getComputerChoice() {
-    const start = Math.floor(Math.random() * 3);
-        if (start === 0) {
-        result = "rock";
-        } else if (start === 1) {
-        result = "paper";
-        } else {
-        result = "scissors";
-        }
-    return result;
-}
-console.log(computerSelection);
+
 /*
 Logic for human choice:
 create a funciton for getHumanChoice
@@ -39,13 +26,7 @@ User input needs case insensitivity
 
 */
 
-function getHumanChoice() {
-        let input = prompt("Choose your weapon!");
-        let human = input.toLowerCase();
-    return human;
-}
 
-console.log(humanSelection);
 
 /*
 Declare player score variables
@@ -53,9 +34,6 @@ Create two new variables named humanScore and computerScore in the GLOBAL SCOPE
 Initialize those variables with a value of 0
 */
 
-let humanScore = 0
-let computerScore = 0
-let tieCounter = 0
 
 /*
 
@@ -71,48 +49,85 @@ rise and repeat for all 3
     
 */
 
-function playRound(humanChoice, computerChoice) {
-    const win = "You win!"
-    const lose = "You lose! Try again!"
-    const tie = "Tie! Try again!"
-
-    if (humanChoice === "rock" && computerChoice === "scissors"){
-        console.log(win);
-        humanScore++;
-        return win;
-    } else if (humanChoice === "rock" && computerChoice === "paper"){
-        console.log(lose);
-        computerScore++;
-        return lose;
-    } else if (humanChoice === "paper" && computerChoice === "rock"){
-        console.log(win);
-        humanScore++;
-        return win;
-    } else if (humanChoice === "paper" && computerChoice === "scissors"){
-        console.log(lose);
-        computerScore++;
-        return lose;
-    } else if (humanChoice === "scissors" && computerChoice === "paper"){
-        console.log(win);
-        humanScore++;
-        return win;
-    } else if (humanChoice === "scissors" && computerChoice === "rock"){
-        console.log(lose);
-        computerScore++;
-        return lose;
-    } else {
-        console.log(tie);
-        return tie;
+function playGame() {
+    let humanScore = 0
+    let computerScore = 0
+    let getHumanChoice = function() {
+        let input = prompt("Choose your weapon!");
+        let human = input.toLowerCase();
+    return human;
     }
+    let getComputerChoice = function() {
+        const start = Math.floor(Math.random() * 3);
+            if (start === 0) {
+            result = "rock";
+            } else if (start === 1) {
+            result = "paper";
+            } else {
+            result = "scissors";
+            }
+        return result;
+    }
+    let playRound = function playRound(humanChoice, computerChoice) {
+        const win = "You win!";
+        const lose = "You lose! Try again!";
+        const tie = "Tie! Try again!";
+    
+        if (humanChoice === "rock" && computerChoice === "scissors") {
+            console.log(win);
+            humanScore++;
+            return win;
+        } else if (humanChoice === "rock" && computerChoice === "paper") {
+            console.log(lose);
+            computerScore++;
+            return lose;
+        } else if (humanChoice === "paper" && computerChoice === "rock") {
+            console.log(win);
+            humanScore++;
+            return win;
+        } else if (humanChoice === "paper" && computerChoice === "scissors") {
+            console.log(lose);
+            computerScore++;
+            return lose;
+        } else if (humanChoice === "scissors" && computerChoice === "paper") {
+            console.log(win);
+            humanScore++;
+            return win;
+        } else if (humanChoice === "scissors" && computerChoice === "rock") {
+            console.log(lose);
+            computerScore++;
+            return lose;
+        } else {
+            console.log(tie);
+            return tie;
+        }
+    }
+
+    for (let i = 0; i < 5; i++) {
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+        console.log(humanScore, computerScore);
+        console.log(humanChoice, computerChoice);
+    } if (humanScore == 5) {
+        alert("You win! Dominating victory!");
+        return;
+    } else if (computerScore == 5) {
+        alert("You lose! Bad luck!");
+        return;
+    }   else if (humanScore > computerScore) {
+        alert("You win! Congratulations!")
+    } else if (humanScore < computerScore) {
+        alert("You lose! Press F5 to try again!");
+    } else if (humanScore === computerScore) {
+        alert("Tie! Press F5 to try again!");
+    }
+    
 }
-
-/*when adding game logic, also add consecutive tie counter
-if tie && tieCounter === 0, 
-tiecounter++, and say "That's 1 tie!"
-if tie && tieCounter > 0, 
-tieCounter++ and say `That's ${tieCounter} ties in a row
-if win || lose, tieCounter = 0
+playGame();
+/*Game Logic:
+Scores are initialized at zero to start
+Need game to play until one player reaches 5
+Repeat playRound() until humanScore or computerScore reaches 5
+At 5, alert a winner, reset Score values to 0 to restart game
 */
-
-// playRound(humanSelection, computerSelection);
-console.log(humanScore, computerScore);
