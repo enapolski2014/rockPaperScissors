7fc093dc2ced6726d3c299d400dfc78feec80f2f
enapolski2014 <enapolski2014@gmail.com>
2026-01-05 23:39:53 -0600

    got closer to win state working, added notes

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # rockPaperScissors
 Rock Paper Scissors project from Odin Project
+
+
+pseudo logic for killing human choice buttons after win
+
+start game, runs play(). Plays a round with playRound(), and checks for win state with 
+    playGame(). playGame() checks if the score is 5 for either player, if it is, remove event listener for buttons. 
--- temp.js
+++ temp.js
@@ -8,7 +8,7 @@ const hSelection = document.getElementById("humanSelection");
 const cSelection = document.getElementById("compSelection");
 const score = document.getElementById("score");
 const winners = document.getElementById("result");
-const game = document.getElementById("game");
+const gameSpace = document.getElementById("game");
 
 let humanScore = 0;
 let computerScore = 0;
@@ -23,49 +23,57 @@ score.textContent = "Click Start to begin the game!";
 
 
 let startButton = document.getElementById('start');
-    start.addEventListener("click", () => {
+    startButton.addEventListener("click", () => {
         play();
         score.textContent = `The score is Human: ${humanScore} to Computer: ${computerScore}`;
         startButton.remove();
+        return;
 });
 
 
 
-
-
-/*function play() {
+function play() {
     hChoiceBtns.forEach((button) => {
-        button.addEventListener("click", () => {
+        button.addEventListener("click", function game() {
             humanChoice = button.id;
             getComputerChoice();
             playRound(humanChoice, computerChoice);
             playGame();
             hSelection.textContent = `Human wields ${button.id}`;
             score.textContent = `The score is Human: ${humanScore =+ humanScore} to Computer: ${computerScore =+ computerScore}`;
+            // The below code works to remove event listener but only for the button clicked
+            //  that causes the win state or for buttons clicked after the win state is 
+            //      achieved. Gotta find a way to make it get rid of all event listeners
+            //          on all buttons once win state is achieved.
+            if (humanScore >= 5 || computerScore >= 5) {
+                hChoiceBtns.forEach((btn) => {
+                    btn.removeEventListener("click", game);
+                });
+            };
         });
     });
-}
-*/
+};
+   
+
+//Checks to see if either player has reached score of 5, if they have, run reset().
+// Haven't figured out a way to kill player buttons in this function yet.
 
 
 function playGame() {
-
-    if (humanScore === 5) {
-        let container = document.getElementById('btnContainer');
-        container.remove();
+    if (humanScore == 5) {
         score.style.fontSize = "1.6rem";
         score.textContent = 'Human score is 5! You win!';
         winners.style.fontSize = "1.6rem";
         winners.textContent = 'Press reset to start again!';
         reset();
-    } else if (computerScore === 5) {
-        let container = document.getElementById('btnContainer');
-        container.remove();
+        return;
+    } else if (computerScore == 5) {
         score.style.fontSize = "1.6rem";
         score.textContent = 'Computer score is 5! Computer wins!';
         winners.style.fontSize = "1.6rem";
         winners.textContent = 'Press reset to start again!';
         reset();
+        return;
     } else {
         return
     }
@@ -73,11 +81,16 @@ function playGame() {
 
 
 
+// reset sets all variables back to how they should be at the beginning of the game. 
+//  like refreshing the page, but without pressing F5. Cant figure out how to remove 
+//      event listeners in this function, maybe actually need to remove buttons in playGame()
+//          and then append them back in this function. FUUUUUUUUCK
+
 function reset() {
+
     let reset = document.createElement("button");
     reset.textContent = "Reset";
-    game.appendChild(reset);
-
+    gameSpace.appendChild(reset);
     reset.addEventListener("click", () => {
         humanScore = 0;
         computerScore = 0;
@@ -87,14 +100,14 @@ function reset() {
         cSelection.textContent = '';
         winners.textContent = '';
         winners.style.fontSize = "1rem";
-        game.removeChild(reset);
-
+        gameSpace.removeChild(reset);
+        play();
     });
-    play();
+
     return;
 };
     
-
+// self explanatory function.
 
 let getComputerChoice = function() {
     const start = Math.floor(Math.random() * 3);
@@ -108,7 +121,10 @@ let getComputerChoice = function() {
         computerChoice = result;
     cSelection.textContent = `Computer wields ${result}`;
     return result;
-}
+};
+
+//playRound() checks between the two inputs to determine winner, increments score for the winner
+// also updates text content
 
 let playRound = function playRound(hum, comp) {
     const win = "You win!";
@@ -145,4 +161,4 @@ let playRound = function playRound(hum, comp) {
     
 
 
-// playRound();
+
